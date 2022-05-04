a4789588554f4e134a37f62e7ac4a6f06111ab2a
Krisztina Komocsin <krisztina.komocsin@gmail.com>
2022-05-04 17:07:03 +0200

    Add files to project and start finding solution

---
+++ index.js
@@ -0,0 +1,21 @@
+/*import fetch from 'node-fetch';
+
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await response.text();
+
+console.log(body);
+
+let images = [
+  {
+    url: 'https://api.memegen.link/images/grumpycat/i_hope_that_what_does_not_kill_you/tries_again.jpg?width=300',
+    dest:
+  },
+  {
+    url: 'https://api.memegen.link/images/cb/i_stole/the_pic--i--nic_basket.jpg?width=300',
+    dest:
+  },
+];
+
+download.images()*/
